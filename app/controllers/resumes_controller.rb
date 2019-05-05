487ab35528bcb191bb0beba8a31b579cfed12c7b
Serene Mathew <serenemathew@Serenes-MacBook-Pro.local>
2019-05-05 21:47:03 +0530

    form for CV

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,40 @@
+class ResumesController < ApplicationController
+  def index
+    @resumes = Resume.all
+  end
+
+  def show
+    @resume = Resume.find(params[:id])
+  end
+
+  def new
+    @resume = Resume.new
+  end
+
+  def edit
+    @resume = Resume.find(params[:id])
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to @resume
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @resume = Resume.find(params[:id])
+    if @resume.update(resume_params)
+      redirect_to @resume
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def resume_params
+      params.require(:resume).permit(:name, :role, :contact_number, :email, :about)
+    end
+end
