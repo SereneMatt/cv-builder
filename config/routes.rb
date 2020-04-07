b704e8700cfc59ee8a72a9905d509e78f7b03922
SereneMatt <serene1990@gmail.com>
2020-04-07 21:01:45 +0200

    Upgraded to Rails 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  get 'home/index'
-  namespace 'api' do
-    namespace 'v1' do
-      root 'home#index'
-
-      resources :users
-      resources :sessions, only: %i[new create destroy]
-
-      get 'signup', to: 'users#new', as: 'signup'
-      get 'login', to: 'sessions#new', as: 'login'
-      get 'logout', to: 'sessions#destroy', as: 'logout'
-      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      resources :resumes
-    end
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
